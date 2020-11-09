ef79f570b28c765304ca490a2096db65cae2c9d1
William Anthony <williamanthony@Williams-MacBook-Pro.local>
2020-11-08 21:45:40 -0800

    scaffolding

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# celesound
+# CELESOUND
 
 ## Project setup
 ```
---
+++ src/components/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="menu">
+    <h1>Menu</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+}
+</script>
+
+<style scoped></style>
