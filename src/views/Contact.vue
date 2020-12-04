0694844c27bc272226312e742f4152b2bc11a229
William Anthony <tony@burrus.me>
2020-12-04 01:08:38 -0800

    validation

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -89,7 +89,11 @@ export default {
 
             var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-            if (regex.test(this.email)) this.isValidForm = true
+            if (regex.test(this.email)) {
+                this.isValidForm = true
+            } else {
+                this.isValidForm = false
+            }
         },
     },
 }
