321daf4e7a402e3c4f01ac3ea0aeecff1c503cdc
William Anthony <tony@burrus.me>
2022-08-27 01:40:08 -0700

    315m

--- src/views/GenesisX.vue
+++ src/views/GenesisX.vue
@@ -9,7 +9,7 @@
                 <a href="https://www.imdb.com/title/tt20836014/fullcredits/?ref_=tt_cl_sm" target="_blank">
                     <h3 id="title">GENESIS X</h3>
                 </a>
-                <iframe width="100%" height="400" src="https://www.youtube-nocookie.com/embed/nYFBphSp4Oo"
+                <iframe width="100%" height="315" src="https://www.youtube-nocookie.com/embed/nYFBphSp4Oo"
                     title="YouTube video player" frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen>
